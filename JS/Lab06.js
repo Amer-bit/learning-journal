29d1457ed5235fc1bec74d10bdfb322d94fb5fea
Amer-bit <https://github.com/Amer-bit>
2020-02-05 09:21:06 +0200

    JS

---
+++ JS/Lab06.js
@@ -0,0 +1,14 @@
+var sue = document.write ("<h2>YEAH I'm all Set</h2>")
+var ast = document.write ("<h2>No Take me to my Mom</h2");
+var rea =confirm("Ready To Dive into SPACE?")
+if (rea == true){
+    sue;
+} else{ast;
+}
+
+
+
+
+
+
+
